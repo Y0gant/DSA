9c94f864d8fad9ebf5a2295879e18d6a7be06e7b
Y0gant <yogantfaye731@proton.me>
2025-01-31 22:33:12 +0530

    Recursion 1

--- programs/src/recursion/Recursion1.java
+++ programs/src/recursion/Recursion1.java
@@ -1,10 +1,15 @@
 package src.recursion;
 
+import java.util.Scanner;
+
 //Simple recursion program
-//to print "Hello World"
+//to print a name using user input
 public class Recursion1 {
     public static void main(String[] args) {
-        printN(5);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of times you want to print the name :");
+        int n = scan.nextInt();
+        printN(n);
 
     }
 

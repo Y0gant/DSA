b2fffbdc71a5e2438100c01495465988bc881e33
Y0gant <yogantfaye731@proton.me>
2025-01-31 22:39:33 +0530

    Recursion 2

---
+++ programs/src/recursion/Recursion2.java
@@ -0,0 +1,18 @@
+package src.recursion;
+
+//Print from 1 to N using Recursion
+public class Recursion2 {
+    public static void main(String[] args) {
+        Recursion2 obj = new Recursion2();
+        obj.printNum(1, 6);
+    }
+
+    public void printNum(int i, int n) {
+        if (i > n) {
+            return;
+        }
+        System.out.println(i);
+        i++;
+        printNum(i, n);
+    }
+}

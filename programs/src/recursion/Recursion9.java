91896c2960d3b6df139c5cd2a910fc50df3a9cb3
Y0gant <yogantfaye731@proton.me>
2025-02-06 18:16:57 +0530

    Recursion 9

---
+++ programs/src/recursion/Recursion9.java
@@ -0,0 +1,25 @@
+package src.recursion;
+
+import java.util.Scanner;
+
+// Check if string is palindrome or not
+public class Recursion9 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter string :");
+        String input = obj.nextLine();
+        System.out.println(checkPalindrome(input, 0));
+    }
+
+    public static boolean checkPalindrome(String str, int start) {
+        if (start >= str.length() / 2) {
+            return true;
+        }
+        if (str.charAt(start) != str.charAt(str.length() - 1 - start)) {
+            return false;
+        }
+
+        return checkPalindrome(str, start + 1);
+
+    }
+}

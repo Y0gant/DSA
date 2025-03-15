9f7d3bd4b41c548466e321d6f897025677294900
Y0gant <yogantfaye731@proton.me>
2025-03-15 22:22:53 +0530

    Second largest element in an array

---
+++ src/main/java/programs/arrays/SecondLargestElement.java
@@ -0,0 +1,30 @@
+package programs.arrays;
+
+public class SecondLargestElement {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {345, 678, 123, 876, 432, 567, 234, 789, 987, 345,
+                654, 111, 222, 333, 444, 555, 777, 888, 999, 101,
+                202, 303, 404, 505, 606, 707, 808, 909, 112, 223,
+                334, 445, 556, 667, 778, 889, 990, 121, 232, 343,
+                454, 565, 676, 787, 898, 919, 212, 323, 434, 545};
+
+        System.out.println("Second largest element: " + secondLargest(arr1));
+       
+    }
+
+    public static int secondLargest(int[] arr) {
+        int largest = arr[0];
+        int secondLargest = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (largest < arr[i]) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] != largest && arr[i] > secondLargest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+}

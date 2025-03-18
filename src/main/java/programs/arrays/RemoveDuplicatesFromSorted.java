039b063d3ebd8d4699b45970cf27ecbabbc4c133
Y0gant <yogantfaye731@proton.me>
2025-03-18 20:54:51 +0530

    Remove duplicates from a sorted array

---
+++ src/main/java/programs/arrays/RemoveDuplicatesFromSorted.java
@@ -0,0 +1,46 @@
+package programs.arrays;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicatesFromSorted {
+    public static void main(String[] args) {
+        int[] nums = {-3, -1, 0, 0, 0, 3, 3};
+        int[] nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int k = removeDuplicates(nums2);
+        int k2 = removeDuplicates2(nums);
+        for (int i = 0; i < k2; i++) {
+            System.out.print(nums[i] + " , ");
+        }
+        System.out.println();
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums2[i] + " , ");
+        }
+    }
+
+    //Bruteforce
+    public static int removeDuplicates(int[] nums) {
+        HashSet<Integer> uniqueElements = new LinkedHashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            uniqueElements.add(nums[i]);
+        }
+        int j = 0;
+        int k = uniqueElements.size();
+        for (int x : uniqueElements) {
+            nums[j++] = x;
+        }
+        return k;
+    }
+
+    public static int removeDuplicates2(int[] nums) {
+        int p = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[p]) {
+                p++;
+                nums[p] = nums[i];
+            }
+        }
+        return ++p;
+    }
+
+}

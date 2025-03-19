3c132f00e5753a6129c405d44fd6d52b820c21f3
Y0gant <yogantfaye731@proton.me>
2025-03-19 17:30:24 +0530

    Rotate array to left by one

---
+++ src/main/java/programs/arrays/RotateByOne.java
@@ -0,0 +1,19 @@
+package programs.arrays;
+
+public class RotateByOne {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        rotateByOne(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void rotateByOne(int[] arr) {
+        int temp = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i - 1] = arr[i];
+        }
+        arr[arr.length - 1] = temp;
+    }
+}

6e6f3f172632ce415bc5dd6a078a3ac53cdd94ec
Y0gant <yogantfaye731@proton.me>
2025-03-27 17:45:57 +0530

    FindTheMissingNumberInAnArray(Optimal)

--- src/main/java/programs/arrays/easy/FindTheMissingNumberInAnArray.java
+++ src/main/java/programs/arrays/easy/FindTheMissingNumberInAnArray.java
@@ -1,12 +1,36 @@
 package programs.arrays.easy;
 
 public class FindTheMissingNumberInAnArray {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 4, 5, 0};
+        int[] arr2 = {3, 0, 2, 4, 5};
+        int[] arr3 = {1, 3, 4, 0};
+        int[] arr4 = {2, 3, 1, 0};
+
+        System.out.println(findMissingBrute(arr1));
+        System.out.println(findMissingBetter(arr2));
+        System.out.println(findMissingOptimal1(arr3));
+        System.out.println(findMissingOptimal2(arr4));
+
+    }
+
+    public static int findMissingOptimal2(int[] nums) {
+//better than the optimal 1 because if n = 10^5, n+1 = (10^5 +1) this will overflow the integer data type,
+// and we are required to use a bigger data type like a long.
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum1 = sum1 ^ (i + 1);
+            sum2 = sum2 ^ nums[i];
+        }
+        return sum1 ^ sum2;
+    }
 
     public static int findMissingOptimal1(int[] nums) {
         //T.C-> O(N)
         //S.C->O(1)
         int n = nums.length;
-        int sum1 = n * (n - 1) / 2;
+        int sum1 = n * (n + 1) / 2;
         int sum2 = 0;
         for (int i = 0; i < nums.length; i++) {
             sum2 += nums[i];
@@ -21,7 +45,8 @@ public class FindTheMissingNumberInAnArray {
         for (int i = 0; i < nums.length; i++) {
             hash[nums[i]]++;
         }
-        for (int i = 0; i < hash.length; i++) {
+        //Numbers ranging from 1 to N
+        for (int i = 1; i < hash.length; i++) {
             if (hash[i] == 0) return i;
         }
         return -1;
@@ -30,6 +55,7 @@ public class FindTheMissingNumberInAnArray {
     public static int findMissingBrute(int[] nums) {
         //T.C-> O(N^2)
         //S.C->O(1)
+        //Range 1 to N
         for (int i = 0; i <= nums.length; i++) {
             int flag = 0;
             for (int j = 0; j < nums.length; j++) {

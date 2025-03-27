5a4005c6f6863c1167ed1788d087cc1b9afb4290
Y0gant <yogantfaye731@proton.me>
2025-03-27 18:55:36 +0530

    Maximum Consecutive One's in an array of 1s and 0s

---
+++ src/main/java/programs/arrays/easy/MaxConsecutiveOne.java
@@ -0,0 +1,22 @@
+package programs.arrays.easy;
+
+public class MaxConsecutiveOne {
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0};
+        System.out.println(maxOnes(arr));
+    }
+
+    public static int maxOnes(int[] arr) {
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                count++;
+                max = Math.max(count, max);
+            } else {
+                count = 0;
+            }
+        }
+        return max;
+    }
+}

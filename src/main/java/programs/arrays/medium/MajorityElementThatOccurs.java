3c63c6089648d46f21e2c6c357aefb7e54ab3169
Y0gant <yogantfaye731@proton.me>
2025-04-04 01:31:30 +0530

    MajorityElementThatOccurs (brute ,better,optimal)

--- src/main/java/programs/arrays/medium/MajorityElementThatOccurs.java
+++ src/main/java/programs/arrays/medium/MajorityElementThatOccurs.java
@@ -3,9 +3,40 @@ package programs.arrays.medium;
 import java.util.HashMap;
 
 public class MajorityElementThatOccurs {
+    public static void main(String[] args) {
+        int[] arrOptimal = {2, 2, 1, 1, 1, 2, 2};
+        int[] arrBetter = {3, 3, 4, 2, 4, 4, 2, 4, 4};
+        int[] arrBrute = {1, 1, 2, 1, 3, 1, 1};
+
+        System.out.println("Majority Element (Optimal): " + findMajorityOptimal(arrOptimal));
+        System.out.println("Majority Element (Better): " + findMajorityBetter(arrBetter));
+        System.out.println("Majority Element (Brute Force): " + findMajorityBrute(arrBrute));
+    }
 
     public static int findMajorityOptimal(int[] arr) {
+        int n = arr.length;
+        int major = n / 2;
+        int count = 0;
+        int element = 0;
 
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
+                count = 1;
+                element = arr[i];
+            } else if (arr[i] == element) {
+                count++;
+            } else count--;
+        }
+
+        int count2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == element) count2++;
+        }
+
+        if (count2 > major) {
+            return element;
+        }
 
         return -1;
     }

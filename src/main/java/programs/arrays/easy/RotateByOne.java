61f2b9659378bece8b47121c6d2a3d8f279b4f87
Y0gant <yogantfaye731@proton.me>
2025-04-18 21:32:33 +0530

    Edge Case update

--- src/main/java/programs/arrays/easy/RotateByOne.java
+++ src/main/java/programs/arrays/easy/RotateByOne.java
@@ -10,6 +10,7 @@ public class RotateByOne {
     }
 
     public static void rotateByOne(int[] arr) {
+        if (arr == null || arr.length <= 1) return;
         int temp = arr[0];
         for (int i = 1; i < arr.length; i++) {
             arr[i - 1] = arr[i];
